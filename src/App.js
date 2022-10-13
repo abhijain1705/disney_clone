f7e86e9ed0e61b5ef044962d8e1392da87e8f8bb
Abhi Jain <abhijain3002@gmail.com>
2022-10-13 16:00:02 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Home from "./components/Home";
+import logo from "./images/logo.png";
+import HomeIcon from "./images/home-icon.svg";
+import MovieIcon from "./images/movie-icon.svg";
+import originalIcon from "./images/original-icon.svg";
+import searchIcon from "./images/search-icon.svg";
+import seriesIcon from "./images/series-icon.svg";
+import watchlistIcon from "./images/watchlist-icon.svg";
 
 function App() {
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">
+        <img alt="logo" height="50px" src={logo} />
+
+        <div className="showNav">
+            <div className="showNavItem">
+              <img alt="nav" src={HomeIcon} />
+              <span>Home</span>
+            </div>
+            <div className="showNavItem">
+              <img alt="nav" src={searchIcon} />
+              <span>SEARCH</span>
+            </div>
+            <div className="showNavItem">
+              <img alt="nav" src={watchlistIcon} />
+              <span>WATCHLIST</span>
+            </div>
+            <div className="showNavItem">
+              <img alt="nav" src={originalIcon} />
+              <span>ORIGINALS</span>
+            </div>
+            <div className="showNavItem">
+              <img alt="nav" src={MovieIcon} />
+              <span>MOVIES</span>
+            </div>
+            <div className="showNavItem">
+              <img alt="nav" src={seriesIcon} />
+              <span>MOVIES</span>
+            </div>
+          </div>
+      </div>
+      <Home />
     </div>
   );
 }
